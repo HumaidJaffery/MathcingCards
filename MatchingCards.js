33d735042d49f8eeff0d7cf7f6e175d2593264e3
HumaidJaffery <humaidjaffery@gmail.com>
2020-08-06 09:52:29 -0700

    CodeCleanUp

--- MatchingCards.js
+++ MatchingCards.js
@@ -1,28 +1,6 @@
 //variables
-var btn1 = document.getElementById('b1');
-var btn2 = document.getElementById('b2');
-var btn3 = document.getElementById('b3');
-var btn4 = document.getElementById('b4');
-var btn5 = document.getElementById('b5');
-var btn6 = document.getElementById('b6');
-var btn7 = document.getElementById('b7');
-var btn8 = document.getElementById('b8');
-var btn9 = document.getElementById('b9');
-var btn10 = document.getElementById('b10');
-var btn11 = document.getElementById('b11');
-var btn12 = document.getElementById('b12');
-var btn13 = document.getElementById('b13');
-var btn14 = document.getElementById('b14');
-var btn15 = document.getElementById('b15');
-var btn16 = document.getElementById('b16');
-var btn17 = document.getElementById('b17');
-var btn18 = document.getElementById('b18');
-var btn19 = document.getElementById('b19');
-var btn20 = document.getElementById('b20');
-var btn21 = document.getElementById('b21');
-var btn22 = document.getElementById('b22');
-var btn23 = document.getElementById('b23');
-var btn24 = document.getElementById('b24');
+var buttonArr = []
+var randomNum = []
 var tryAgainBtn = document.getElementById('tryAgainId');
 var menuBtn = document.getElementById('menuId');
 var button = document.getElementById('buttons');
@@ -32,156 +10,57 @@ let checkBtnArr = []
 var count = 0;
 var sec = 0; 
 var numbers = ['1', '2', '3', '4','1', '2', '3', '4',]
-var buttonArr = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8]
-var hardBtnArr = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23, btn24]
-var mediumBtnArr = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16]
 var pressed = false
-var randomNum1;
-var randomNum2;
-var randomNum3;
-var randomNum4;
-var randomNum5;
-var randomNum6;
-var randomNum7;
-var randomNum8;
-var randomNum9;
-var randomNum10;
-var randomNum11;
-var randomNum12;
-var randomNum13;
-var randomNum14;
-var randomNum15;
-var randomNum16;
-var randomNum17;
-var randomNum18;
-var randomNum19;
-var randomNum20;
-var randomNum21;
-var randomNum22;
-var randomNum23;
-var randomNum24;
+var btnNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+var safeBtnArr = [];
+
 
 
 function done(){
 if(difficulty.className == 'medium'){
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8']
-    buttonArr = mediumBtnArr
-    for(i=0; i<mediumBtnArr.length; i++){
-        mediumBtnArr[i].hidden = ''
+    for (i=1; i<17; i++) {
+        buttonArr.push(document.getElementById('b'+i));
+        safeBtnArr.push(document.getElementById('b'+i));
+        randomNum.push(numbers[Math.floor(Math.random() * numbers.length )]) 
+        removeNum(randomNum[i-1])
+        var done = true
     }
-    randomNum1 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum1)
-    randomNum2 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum2)
-    randomNum3 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum3)
-    randomNum4 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum4)
-    randomNum5 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum5)
-    randomNum6 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum6)
-    randomNum7 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum7)
-    randomNum8 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum8)
-    randomNum9 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum9)
-    randomNum10 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum10)
-    randomNum11 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum11)
-    randomNum12 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum12)
-    randomNum13 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum13)
-    randomNum14 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum14)
-    randomNum15 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum15)
-    randomNum16 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum16)
+    if(done == true){
+        for(i=0;i<buttonArr.length; i++){
+            buttonArr[i].hidden = ''
+        }
+    }
 
 } else if(difficulty.className == 'hard'){
-    buttonArr = hardBtnArr
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8',  '9',  '10', '11', '12','1', '2', '3', '4', '5', '6', '7', '8',  '9',  '10', '11', '12']
-    for(i=0; i<hardBtnArr.length; i++){
-        hardBtnArr[i].hidden = ''
+    for (i=1; i < 25; i++) {
+        buttonArr.push(document.getElementById('b'+i));
+        safeBtnArr.push(document.getElementById('b'+i));
+        randomNum.push(numbers[Math.floor(Math.random() * numbers.length )]) 
+        removeNum(randomNum[i-1])
+        var done = true
     }
-    randomNum1 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum1)
-    randomNum2 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum2)
-    randomNum3 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum3)
-    randomNum4 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum4)
-    randomNum5 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum5)
-    randomNum6 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum6)
-    randomNum7 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum7)
-    randomNum8 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum8)
-    randomNum9 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum9)
-    randomNum10 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum10)
-    randomNum11 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum11)
-    randomNum12 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum12)
-    randomNum13 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum13)
-    randomNum14 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum14)
-    randomNum15 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum15)
-    randomNum16 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum16)
-    randomNum17 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum17)
-    randomNum18 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum18)
-    randomNum19 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum19)
-    randomNum20 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum20)
-    randomNum21 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum21)
-    randomNum22 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum22)
-    randomNum23 = numbers[Math.floor(Math.random() * numbers.length)] 
-    removeNum(randomNum23)
-    randomNum24 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum24)
+    if(done == true){
+        for(i=0;i<buttonArr.length; i++){
+            buttonArr[i].hidden = ''
+        }
+    }
 
 } else if(difficulty.className = 'easy'){
-    randomNum1 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum1)
-    randomNum2 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum2)
-    randomNum3 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum3)
-    randomNum4 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum4)
-    randomNum5 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum5)
-    randomNum6 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum6)
-    randomNum7 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum7)
-    randomNum8 = numbers[Math.floor(Math.random() * numbers.length)]
-    removeNum(randomNum8)
-}
+    for (i=1; i < 9; i++) {
+        buttonArr.push(document.getElementById('b'+i));
+        safeBtnArr.push(document.getElementById('b'+i));
+        randomNum.push(numbers[Math.floor(Math.random() * numbers.length )]) 
+        removeNum(randomNum[i-1])
+    }
 
+}
     options.hidden = 'hidden'
     button.hidden = ''
-    pressed = true
-    
-
+    pressed = true 
 }
+
 
 function removeNum(Num){
     numbers.splice(numbers.indexOf(Num), 1)
@@ -216,6 +95,7 @@ function reveal(btn, randomNum){
     btn.className = 'open'
     btn.innerHTML = randomNum
     checkBtnArr.push(btn)
+
     }
 }
 
@@ -268,3 +148,4 @@ function timer(){
     }
 }
 const time = setInterval(timer, 1000)
+
